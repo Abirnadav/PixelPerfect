e02510aa33752bc2b9d5578bf2e9785574aea4f0
AbirNadav <pakakaw41@gmail.com>
2020-06-21 15:40:33 +0300

    Updates

---
+++ precache-manifest.6056a1633d57a5907362029a541184d2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bcf5514d68bc3890869a459ad60710",
+    "url": "/PixelPerfect/index.html"
+  },
+  {
+    "revision": "1b8c91ed5ea14a095c8f",
+    "url": "/PixelPerfect/static/css/main.b1257ada.chunk.css"
+  },
+  {
+    "revision": "da53e5dd189e7ce06ca4",
+    "url": "/PixelPerfect/static/js/2.6c99fdad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PixelPerfect/static/js/2.6c99fdad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8c91ed5ea14a095c8f",
+    "url": "/PixelPerfect/static/js/main.c2ee6007.chunk.js"
+  },
+  {
+    "revision": "81d33a655456b7f63b84",
+    "url": "/PixelPerfect/static/js/runtime-main.f58e8c80.js"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/PixelPerfect/static/media/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/PixelPerfect/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/PixelPerfect/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/PixelPerfect/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/PixelPerfect/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/PixelPerfect/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/PixelPerfect/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PixelPerfect/static/media/logo.5d5d9eef.svg"
+  }
+]);
